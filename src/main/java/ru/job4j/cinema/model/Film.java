2661cc6c862d8442ec43861d3b748b8bdee4eb86
apollobing <taras.career@gmail.com>
2024-03-31 14:15:57 +0200

    feat: add models - 2. Сервис - Кинотеатр [#504869]

---
+++ src/main/java/ru/job4j/cinema/model/Film.java
@@ -0,0 +1,179 @@
+package ru.job4j.cinema.model;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class Film {
+
+    public static final Map<String, String> COLUMN_MAPPING = Map.of(
+            "id", "id",
+            "name", "name",
+            "description", "description",
+            "year", "year",
+            "genre_id", "genreId",
+            "minimal_age", "minimalAge",
+            "duration_in_minutes", "durationInMinutes",
+            "file_id", "fileId"
+    );
+
+    private int id;
+
+    private String name;
+
+    private String description;
+
+    private int year;
+
+    private int genreId;
+
+    private int minimalAge;
+
+    private int durationInMinutes;
+
+    private int fileId;
+
+    public static class Builder {
+        private int id;
+        private String name;
+        private String description;
+        private int year;
+        private int genreId;
+        private int minimalAge;
+        private int durationInMinutes;
+        private int fileId;
+
+        public Builder buildId(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder buildName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder buildDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder buildYear(int year) {
+            this.year = year;
+            return this;
+        }
+
+        public Builder buildGenreId(int genreId) {
+            this.genreId = genreId;
+            return this;
+        }
+
+        public Builder buildMinimalAge(int minimalAge) {
+            this.minimalAge = minimalAge;
+            return this;
+        }
+
+        public Builder buildDurationInMinutes(int durationInMinutes) {
+            this.durationInMinutes = durationInMinutes;
+            return this;
+        }
+
+        public Builder buildFileId(int fileId) {
+            this.fileId = fileId;
+            return this;
+        }
+
+        public Film build() {
+            Film film = new Film();
+            film.id = id;
+            film.name = name;
+            film.description = description;
+            film.year = year;
+            film.genreId = genreId;
+            film.minimalAge = minimalAge;
+            film.durationInMinutes = durationInMinutes;
+            film.fileId = fileId;
+            return film;
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getGenreId() {
+        return genreId;
+    }
+
+    public void setGenreId(int genreId) {
+        this.genreId = genreId;
+    }
+
+    public int getMinimalAge() {
+        return minimalAge;
+    }
+
+    public void setMinimalAge(int minimalAge) {
+        this.minimalAge = minimalAge;
+    }
+
+    public int getDurationInMinutes() {
+        return durationInMinutes;
+    }
+
+    public void setDurationInMinutes(int durationInMinutes) {
+        this.durationInMinutes = durationInMinutes;
+    }
+
+    public int getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(int fileId) {
+        this.fileId = fileId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Film film = (Film) o;
+        return id == film.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
