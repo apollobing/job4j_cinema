d190e68f0fb51146a834eae1a394d9e657518e9e
apollobing <taras.career@gmail.com>
2024-04-01 15:23:44 +0200

    feat: implement image output - 2. Сервис - Кинотеатр [#504869]

--- src/main/java/ru/job4j/cinema/service/SimpleFileService.java
+++ src/main/java/ru/job4j/cinema/service/SimpleFileService.java
@@ -1,9 +1,12 @@
 package ru.job4j.cinema.service;
 
 import org.springframework.stereotype.Service;
-import ru.job4j.cinema.model.File;
+import ru.job4j.cinema.dto.FileDto;
 import ru.job4j.cinema.repository.FileRepository;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Optional;
 
 @Service
@@ -16,7 +19,20 @@ public class SimpleFileService implements FileService {
     }
 
     @Override
-    public Optional<File> findById(int id) {
-        return fileRepository.findById(id);
+    public Optional<FileDto> findById(int id) {
+        var fileOptional = fileRepository.findById(id);
+        if (fileOptional.isEmpty()) {
+            return Optional.empty();
+        }
+        var content = readFileAsBytes(fileOptional.get().getPath());
+        return Optional.of(new FileDto(fileOptional.get().getName(), content));
+    }
+
+    private byte[] readFileAsBytes(String path) {
+        try {
+            return Files.readAllBytes(Path.of(path));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
