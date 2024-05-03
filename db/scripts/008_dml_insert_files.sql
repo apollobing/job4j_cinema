0ff8bd7e26d073a6f509581b8e0b970667fe2cb0
apollobing <taras.career@gmail.com>
2024-05-03 20:30:12 +0200

    fix: change path of files in script - 2. Сервис - Кинотеатр [#504869]

--- db/scripts/008_dml_insert_files.sql
+++ db/scripts/008_dml_insert_files.sql
@@ -1,40 +1,40 @@
 insert into files(id, name, path)
-    values(1, 'avatar.jpg', 'files\avatar.jpg');
+    values(1, 'avatar.jpg', 'files/avatar.jpg');
 insert into files(id, name, path)
-    values(2, 'forrest-gump.jpg', 'files\forrest-gump.jpg');
+    values(2, 'forrest-gump.jpg', 'files/forrest-gump.jpg');
 insert into files(id, name, path)
-    values(3, 'gladiator.jpg', 'files\gladiator.jpg');
+    values(3, 'gladiator.jpg', 'files/gladiator.jpg');
 insert into files(id, name, path)
-    values(4, 'home-alone.jpg', 'files\home-alone.jpg');
+    values(4, 'home-alone.jpg', 'files/home-alone.jpg');
 insert into files(id, name, path)
-    values(5, 'inception.jpg', 'files\inception.jpg');
+    values(5, 'inception.jpg', 'files/inception.jpg');
 insert into files(id, name, path)
-    values(6, 'interstellar.jpg', 'files\interstellar.jpg');
+    values(6, 'interstellar.jpg', 'files/interstellar.jpg');
 insert into files(id, name, path)
-    values(7, 'it.jpg', 'files\it.jpg');
+    values(7, 'it.jpg', 'files/it.jpg');
 insert into files(id, name, path)
-    values(8, 'se7en.jpg', 'files\se7en.jpg');
+    values(8, 'se7en.jpg', 'files/se7en.jpg');
 insert into files(id, name, path)
-    values(9, 'the-conjuring.jpg', 'files\the-conjuring.jpg');
+    values(9, 'the-conjuring.jpg', 'files/the-conjuring.jpg');
 insert into files(id, name, path)
-    values(10, 'the-dark-knight.jpg', 'files\the-dark-knight.jpg');
+    values(10, 'the-dark-knight.jpg', 'files/the-dark-knight.jpg');
 insert into files(id, name, path)
-    values(11, 'the-departed.jpg', 'files\the-departed.jpg');
+    values(11, 'the-departed.jpg', 'files/the-departed.jpg');
 insert into files(id, name, path)
-    values(12, 'the-godfather.jpg', 'files\the-godfather.jpg');
+    values(12, 'the-godfather.jpg', 'files/the-godfather.jpg');
 insert into files(id, name, path)
-    values(13, 'the-green-mile.jpg', 'files\the-green-mile.jpg');
+    values(13, 'the-green-mile.jpg', 'files/the-green-mile.jpg');
 insert into files(id, name, path)
-    values(14, 'the-hangover.jpg', 'files\the-hangover.jpg');
+    values(14, 'the-hangover.jpg', 'files/the-hangover.jpg');
 insert into files(id, name, path)
-    values(15, 'the-lion-king.jpg', 'files\the-lion-king.jpg');
+    values(15, 'the-lion-king.jpg', 'files/the-lion-king.jpg');
 insert into files(id, name, path)
-    values(16, 'the-prestige.jpg', 'files\the-prestige.jpg');
+    values(16, 'the-prestige.jpg', 'files/the-prestige.jpg');
 insert into files(id, name, path)
-    values(17, 'the-shawshank-redemption.jpg', 'files\the-shawshank-redemption.jpg');
+    values(17, 'the-shawshank-redemption.jpg', 'files/the-shawshank-redemption.jpg');
 insert into files(id, name, path)
-    values(18, 'the-silence-of-the-lambs.jpg', 'files\the-silence-of-the-lambs.jpg');
+    values(18, 'the-silence-of-the-lambs.jpg', 'files/the-silence-of-the-lambs.jpg');
 insert into files(id, name, path)
-    values(19, 'the-sixth-sense.jpg', 'files\the-sixth-sense.jpg');
+    values(19, 'the-sixth-sense.jpg', 'files/the-sixth-sense.jpg');
 insert into files(id, name, path)
-    values(20, 'toy-story.jpg', 'files\toy-story.jpg');
+    values(20, 'toy-story.jpg', 'files/toy-story.jpg');
